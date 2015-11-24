73bbaf7138bf4ecca9cdd581dadb508bd3dca676
kongou-ae <y.matsumoto.ae@gmail.com>
2015-11-24 23:11:44 +0900

    update test script

--- test-redpen.sh
+++ test-redpen.sh
@@ -3,8 +3,9 @@ mv redpen/blog.xml redpen-*/conf
 mv redpen/spellCheck.js redpen-*/js
 
 filename=`git diff HEAD^ HEAD --name-only`
+echo $filename
 
-if [[ $filename =~ .*.md$ ]] ;then
+if [[ $filename =~ .*\.md$ ]] ;then
     echo "testing...$filename"
     redpen-*/bin/redpen -c conf/blog.xml -f markdown aimless.jp/content/blog/archives/$filename
 fi
