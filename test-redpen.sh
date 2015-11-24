a343aac4d8435d22b0e89a2249c0449283d44a22
kongou-ae <y.matsumoto.ae@gmail.com>
2015-11-24 21:54:56 +0900

    try to add redpen CI

---
+++ test-redpen.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+mv redpen/blog.xml redpen-*/conf
+mv redpen/spellCheck.js redpen-*/js
+
+filename=`git diff --name-only`
+
+if [[ ! $filename =~ .*.md$ ]] ;then
+    redpen-*/bin/redpen -c conf/blog.xml -f markdown aimless.jp/content/blog/archives/$filename
