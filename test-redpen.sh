0238e4641b90b17727e9a67928c1bc02cee252bc
kongou-ae <y.matsumoto.ae@gmail.com>
2015-11-25 22:35:44 +0900

    mdではないときのメッセージを追加

--- test-redpen.sh
+++ test-redpen.sh
@@ -3,9 +3,10 @@ mv redpen/blog.xml redpen-*/conf
 mv redpen/spellCheck.js redpen-*/js
 
 filename=`git diff HEAD^ HEAD --name-only`
-echo $filename
 
 if [[ $filename =~ .*\.md$ ]] ;then
-    echo "testing...$filename"
+    echo "testing $filename..."
     redpen-*/bin/redpen -c redpen-*/conf/blog.xml -f markdown $filename
+else
+    echo "$filename is not markdown. The test will not be performed."
 fi
