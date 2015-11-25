6211688980de6d7a68bf36ad84fce875761e83a2
kongou-ae <y.matsumoto.ae@gmail.com>
2015-11-25 22:41:24 +0900

    add the message

--- test-redpen.sh
+++ test-redpen.sh
@@ -5,7 +5,7 @@ mv redpen/spellCheck.js redpen-*/js
 filename=`git diff HEAD^ HEAD --name-only`
 
 if [[ $filename =~ .*\.md$ ]] ;then
-    echo "testing $filename..."
+    echo "start to test $filename...."
     redpen-*/bin/redpen -c redpen-*/conf/blog.xml -f markdown $filename
 else
     echo "$filename is not markdown. The test will not be performed."
