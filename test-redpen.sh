d35263eba43632fb086ea86e8255c3feb664c2c4
kongou-ae <y.matsumoto.ae@gmail.com>
2016-04-24 19:05:22 +0900

    modify redpen [skip test ]

--- test-redpen.sh
+++ test-redpen.sh
@@ -4,9 +4,15 @@ mv redpen/spellCheck.js redpen-*/js
 
 filename=`git diff HEAD^ HEAD --name-only`
 
+commitMassage=`git log -n 1 --oneline --pretty=format:"%s"`
+
 if [[ $filename =~ .*\.md$ ]] ;then
-    echo "start to test $filename...."
-    redpen-*/bin/redpen -c redpen-*/conf/blog.xml -f markdown $filename
+    if [[ "$commitMassage" =~ \[skip test\] ]]; then
+        echo "redpen test is skipped"
+    else
+        echo "start to test $filename...."
+        redpen-*/bin/redpen -c redpen-*/conf/blog.xml -f markdown $filename
+    fi
 else
-    echo "$filename is not markdown. The test will not be performed."
+    echo "$filename is not markdown. Redpen test is skipped."
 fi
